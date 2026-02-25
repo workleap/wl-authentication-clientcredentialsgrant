b382ae9d86dab083ab7a4665f4597a5a8caa7282
Gérald Barré <gerald.barre@gsoft.com>
2026-02-25 12:25:28 -0500

    Upgrade Swashbuckle.AspNetCore to 10.1.4

--- src/tests/WebApi.OpenAPI.SystemTest/Endpoints/ClientCredentialMinimalApis.cs
+++ src/tests/WebApi.OpenAPI.SystemTest/Endpoints/ClientCredentialMinimalApis.cs
@@ -9,7 +9,6 @@ public static class ClientCredentialMinimalApis
         app.MapGet("/minimal-api", () => "Hello World")
             .WithSummary("This minimal API should require the cocktail.make permission.")
             .RequireClientCredentials("cocktail.make")
-            .WithTags("ClientCredentials")
-            .WithOpenApi();
+            .WithTags("ClientCredentials");
     }
 }
